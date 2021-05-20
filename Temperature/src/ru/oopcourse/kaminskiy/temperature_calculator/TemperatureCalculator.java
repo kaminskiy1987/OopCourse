a43ad8b587a0ce37d66e5a71a8ad1ce6ecd7404e
kaminskiy1987 <72153000+kaminskiy1987@users.noreply.github.com>
2021-05-20 19:54:08 +0700

    Update TemperatureCalculator.java

--- Temperature/src/ru/oopcourse/kaminskiy/temperature_calculator/TemperatureCalculator.java
+++ Temperature/src/ru/oopcourse/kaminskiy/temperature_calculator/TemperatureCalculator.java
@@ -3,9 +3,9 @@ package ru.oopcourse.kaminskiy.temperature_calculator;
 import ru.oopcourse.kaminskiy.temperature_scale.TemperatureScale;
 
 public class TemperatureCalculator {
-    public double convert(double temperature, TemperatureScale temperatureScaleToCelsius, TemperatureScale temperatureScaleFromCelsius) {
-        double temperatureConversionToCelsius = temperatureScaleToCelsius.toCelsius(temperature);
+    public double convert(double temperature, TemperatureScale scaleFromWhichTransfer, TemperatureScale scaleToWhichTransfer) {
+        double temperatureInCelsius = scaleFromWhichTransfer.toCelsius(temperature);
 
-        return temperatureScaleFromCelsius.fromCelsius(temperatureConversionToCelsius);
+        return scaleToWhichTransfer.fromCelsius(temperatureInCelsius);
     }
 }
